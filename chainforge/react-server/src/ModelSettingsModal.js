074946909202eb5ff8d8bbfd5689771a184e1b75
Ian Arawjo <fatso784@gmail.com>
2023-06-07 19:39:58 -0400

    Fix settings modal bug in FireFox

--- chainforge/react-server/src/ModelSettingsModal.js
+++ chainforge/react-server/src/ModelSettingsModal.js
@@ -13,7 +13,7 @@ import { ModelSettings, getDefaultModelFormData, postProcessFormData } from './M
 
 const ModelSettingsModal = forwardRef((props, ref) => {
   const [opened, { open, close }] = useDisclosure(false);
-  const form = useRef(null);
+
   const [formData, setFormData] = useState(undefined);
   const onSettingsSubmit = props.onSettingsSubmit;
   const selectedModelKey = props.model ? props.model.key : null;
@@ -58,36 +58,41 @@ const ModelSettingsModal = forwardRef((props, ref) => {
     return postProcessFormData(ModelSettings[props.model.base_model], fdata);
   }, [props.model]);
 
-  const saveFormState = useCallback(() => {
-    if (form && form.current)
-        form.current.submit();
-  }, [form]);
-
-  const onSubmit = useCallback((submitInfo) => {
-    setFormData(submitInfo.formData);
+  const saveFormState = useCallback((fdata) => {
+    setFormData(fdata);
 
     if (onSettingsSubmit) {
         props.model.emoji = modelEmoji;
-        onSettingsSubmit(props.model, submitInfo.formData, postprocess(submitInfo.formData));
+        onSettingsSubmit(props.model, fdata, postprocess(fdata));
     }
+  }, [props, modelEmoji, setFormData, onSettingsSubmit, postprocess]);
 
-  }, [close, setFormData, modelEmoji, onSettingsSubmit, selectedModelKey]);
+  const onSubmit = useCallback((submitInfo) => {
+    saveFormState(submitInfo.formData);
+  }, [saveFormState]);
+
+  const onFormDataChange = (state) => {
+    if (state && state.formData)
+      setFormData(state.formData);
+  };
 
   const onClickSubmit = useCallback(() => {
-    if (form && form.current)
-        form.current.submit();
+    if (formData)
+        saveFormState(formData);
     close();
-  }, [form]);
+  }, [formData, close, saveFormState]);
 
-  const onEmojiSelect = (selection) => {
-    setModelEmoji(selection.native);
+  const onEmojiSelect = useCallback((selection) => {
+    const emoji = selection.native;
+    setModelEmoji(emoji);
     setEmojiPickerOpen(false);
-  };
+    console.log('picked', emoji);
+  }, [setModelEmoji, setEmojiPickerOpen]);
 
   // This gives the parent access to triggering the modal
   const trigger = useCallback(() => {
     open();
-  }, [schema, uiSchema, modelName]);
+  }, [schema, uiSchema, modelName, open]);
   useImperativeHandle(ref, () => ({
     trigger,
   }));
@@ -97,7 +102,7 @@ return (
         <div>
             <Popover width={200} position="bottom" withArrow shadow="md" withinPortal className="nowheel nodrag" opened={emojiPickerOpen} onChange={setEmojiPickerOpen}>
                 <Popover.Target>
-                    <Button variant='subtle' compact style={{fontSize: '16pt'}} onClick={() => {setEmojiPickerOpen((o) => !o); saveFormState();}}>{modelEmoji}</Button>
+                    <Button variant='subtle' compact style={{fontSize: '16pt'}} onClick={() => {setEmojiPickerOpen((o) => !o);}}>{modelEmoji}</Button>
                 </Popover.Target>
                 <Popover.Dropdown>
                     <Picker data={emojidata} onEmojiSelect={onEmojiSelect} theme="light" />
@@ -106,7 +111,7 @@ return (
         </div>
     } closeOnClickOutside={false} style={{position: 'relative', 'left': '-100px'}}>
         
-        <Form ref={form} schema={schema} uiSchema={uiSchema} formData={formData} validator={validator} onSubmit={onSubmit} style={{width: '100%'}}>
+        <Form schema={schema} uiSchema={uiSchema} formData={formData} validator={validator} onChange={onFormDataChange} onSubmit={onSubmit} style={{width: '100%'}}>
             <Button title='Submit' onClick={onClickSubmit} style={{float: 'right', marginRight: '30px'}}>Submit</Button>
             <div style={{height: '50px'}}></div>
         </Form>
