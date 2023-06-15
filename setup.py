ecb76d136950d3a1f843e5a63f069826d3ebf683
Ian Arawjo <fatso784@gmail.com>
2023-06-15 14:16:18 -0400

    Update package info and point oaievals to main branch

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(
     name='chainforge',
-    version='0.1.5.3',
+    version='0.1.6',
     packages=find_packages(),
     author="Ian Arawjo",
     description="A Visual Programming Environment for Prompt Engineering",
