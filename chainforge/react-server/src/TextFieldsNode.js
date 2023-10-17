b47e7c7359528ee95a230539825920287a5dc3da
Sean Yang <53060248+shawseanyang@users.noreply.github.com>
2023-10-16 21:15:00 -0400

    Cosmetic changes

--- chainforge/react-server/src/TextFieldsNode.js
+++ chainforge/react-server/src/TextFieldsNode.js
@@ -321,13 +321,7 @@ const TextFieldsNode = ({ data, id }) => {
   )
 
   const aiPopover = (
-    <Tabs variant="outline" color="grape" defaultValue="extend" inverted>
-      <Tabs.Panel value="extend" pb="xs">
-        {extendUI}
-      </Tabs.Panel>
-      <Tabs.Panel value="replace" pb="xs">
-        {replaceUI}
-      </Tabs.Panel>
+    <Tabs color="grape" defaultValue="extend">
       <Tabs.List grow>
         <Tabs.Tab value="extend">
           Extend
@@ -336,6 +330,12 @@ const TextFieldsNode = ({ data, id }) => {
           Replace
         </Tabs.Tab>
       </Tabs.List>
+      <Tabs.Panel value="extend" pb="xs">
+        {extendUI}
+      </Tabs.Panel>
+      <Tabs.Panel value="replace" pb="xs">
+        {replaceUI}
+      </Tabs.Panel>
     </Tabs>
   )
 
