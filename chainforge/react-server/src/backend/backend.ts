a861695c8799317528747a89f264fbe2c9e52aad
Ian Arawjo <fatso784@gmail.com>
2023-11-26 21:48:36 -0500

    Make response reordering more efficient

--- chainforge/react-server/src/backend/backend.ts
+++ chainforge/react-server/src/backend/backend.ts
@@ -721,14 +721,22 @@ export async function queryLLM(id: string,
   let res = Object.values(responses).flatMap(rs => rs.map(to_standard_format));
 
   // Reorder the responses to match the original vars dict ordering of keys and values
+  let vars_lookup = {}; // we create a lookup table for faster sort
+  Object.entries(vars).forEach(([varname, vals]) => {
+    vars_lookup[varname] = {};
+    vals.forEach((vobj: Dict | string, i: number) => {
+      const v = typeof vobj === "string" ? vobj : vobj?.text;
+      vars_lookup[varname][v] = i;
+    });
+  });
   res.sort((a, b) => {
     if (!a.vars || !b.vars) return 0;
-    for (const [varname, vals] of Object.entries(vars)) {
+    for (const [varname, vals] of Object.entries(vars_lookup)) {
       if (varname in a.vars && varname in b.vars) {
         const a_val = a.vars[varname];
         const b_val = b.vars[varname];
-        const a_idx = vals.findIndex(v => v === a_val || v?.text === a_val);
-        const b_idx = vals.findIndex(v => v === b_val || v?.text === b_val);
+        const a_idx = vals[a_val];
+        const b_idx = vals[b_val];
         if (a_idx > -1 && b_idx > -1 && a_idx !== b_idx) 
           return a_idx - b_idx;
       }
