f399c84bec4ed33c80e1e4d49d17d0886860090b
Ian Arawjo <fatso784@gmail.com>
2024-01-13 18:19:26 -0500

    Rebuild app and update package version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(
     name='chainforge',
-    version='0.2.8.9',
+    version='0.2.9.0',
     packages=find_packages(),
     author="Ian Arawjo",
     description="A Visual Programming Environment for Prompt Engineering",
