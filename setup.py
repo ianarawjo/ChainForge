dec70703e05d3d13da44801fcb2fc70658400ca6
Ian Arawjo <fatso784@gmail.com>
2024-03-07 14:30:46 -0500

    Bug fix code on save

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(
     name='chainforge',
-    version='0.3.0.0',
+    version='0.3.0.1',
     packages=find_packages(),
     author="Ian Arawjo",
     description="A Visual Programming Environment for Prompt Engineering",
