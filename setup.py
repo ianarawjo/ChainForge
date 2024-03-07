d90a6455057e0b3b0bb38285a215efa104c3fddb
Ian Arawjo <fatso784@gmail.com>
2024-03-07 15:36:40 -0500

    Clean escaped braces before eval

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(
     name='chainforge',
-    version='0.3.0.1',
+    version='0.3.0.2',
     packages=find_packages(),
     author="Ian Arawjo",
     description="A Visual Programming Environment for Prompt Engineering",
