e3259ecc1bc4b4ef5911b82115f718d3e2e5e690
Ian Arawjo <fatso784@gmail.com>
2024-05-14 06:59:33 -1000

    Add new OpenAI models

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(
     name='chainforge',
-    version='0.3.1.8',
+    version='0.3.1.9',
     packages=find_packages(),
     author="Ian Arawjo",
     description="A Visual Programming Environment for Prompt Engineering",
