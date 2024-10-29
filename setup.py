e9da6bc1b2c847a7173328bcdfa02e80ea1cc848
Sam <sammcj@users.noreply.github.com>
2024-10-29 17:52:45 +1100

    chore(deps): bump deps, python versions

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ def readme():
         return f.read()
 
 setup(
-    name='chainforge',
-    version='0.3.2.1',
+    name="chainforge",
+    version="0.3.2.2",
     packages=find_packages(),
     author="Ian Arawjo",
     description="A Visual Programming Environment for Prompt Engineering",
     long_description=readme(),
-    long_description_content_type='text/markdown',
-    keywords='prompt engineering LLM response evaluation',
+    long_description_content_type="text/markdown",
+    keywords="prompt engineering LLM response evaluation",
     license="MIT",
     url="https://github.com/ianarawjo/ChainForge/",
     install_requires=[
@@ -29,19 +29,21 @@ setup(
         "mistune>=2.0",  # for LLM response markdown parsing
     ],
     entry_points={
-        'console_scripts': [
-            'chainforge = chainforge.app:main',
+        "console_scripts": [
+            "chainforge = chainforge.app:main",
         ],
     },
     classifiers=[
         # Package classifiers
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     python_requires=">=3.8",
     include_package_data=True,
