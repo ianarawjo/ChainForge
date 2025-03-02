3b929880dc5a35aafa546dc6f2820ae92c5ee141
Ian Arawjo <fatso784@gmail.com>
2025-03-02 10:44:48 -0500

    Update package version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(
     name="chainforge",
-    version="0.3.4.1",
+    version="0.3.4.2",
     packages=find_packages(),
     author="Ian Arawjo",
     description="A Visual Programming Environment for Prompt Engineering",
